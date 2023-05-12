ca065b4f3ccafec626ee476d4abcb70b155ad7ff
anielrussel <anielrussel13@gmail.com>
2023-05-12 11:15:06 +0800

    "fixed routing"

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,43 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import InventoryForm from "../components/InventoryForm";
 import useInventoryContext from "../hooks/useInventoryContext";
 import axios from "axios";
 import InventoryDetails from "../components/InventoryDetails";
-import useAuthContext from "../hooks/useAuthContext";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import useAuthContext from "../hooks/useAuthContext";
+import Login from "./Login";
 
 const Home: React.FC = () => {
   const { inventory, dispatch } = useInventoryContext();
   const { user } = useAuthContext();
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchInventory = async () => {
+      try {
         const response = await axios.get("https://inventory-sytem-mern.onrender.com/api/inventory", {
           headers: {
-            "Authorization": `Bearer ${user.token}`,
+            Authorization: `Bearer ${user.token}`,
           },
         });
         if (response.status === 200) {
           dispatch({ type: "SET_INVENTORY", payload: response.data });
         }
+      } catch (error) {
+        console.error("Error fetching inventory:", error);
+      } finally {
+        setLoading(false);
+      }
     };
-    if (user) {
-      fetchInventory()
+
+    if (user && user.token) {
+      fetchInventory();
+    } else {
+      setLoading(false);
     }
   }, [dispatch, user]);
 
+  if (loading) {
+    return <div className="flex justify-center items-center h-screen text-center"><h1 className="font-Rubik text-3xl font-bold">Loading...</h1></div>;
+  }
+
   return (
     <div>
-          <Navbar />
-          <div className="px-4 lg:px-32 mt-32">
-            <div>
-              <InventoryForm />
+      {inventory
+        ? (
+          <>
+            <Navbar />
+            <div className="px-4 lg:px-32 mt-32">
+              <div>
+                <InventoryForm />
+              </div>
+              <InventoryDetails inventory={inventory} />
             </div>
-            {inventory && inventory.map((inventories) =>  <InventoryDetails key={inventories._id} inventory={inventory} />)}
-          </div>
-          <Footer />
+            <Footer />
+          </>
+        )
+        : 
+        <Login />
+      }
     </div>
+
   );
 };
 
